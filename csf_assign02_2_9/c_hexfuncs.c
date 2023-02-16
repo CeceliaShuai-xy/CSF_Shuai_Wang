2883e3a4e33d28b16364a8287f7bd448bb8582de
Yiran Wang <you@somewhere.com>
2023-02-15 22:15:13 -0500

    start assm, refactor hex_format_byte_as_hex

--- csf_assign02_2_9/c_hexfuncs.c
+++ csf_assign02_2_9/c_hexfuncs.c
@@ -10,7 +10,11 @@ unsigned hex_read(char data_buf[]) {
 }
 
 void hex_write_string(const char s[]) {
-    write(1, s, sizeof(s)-1);
+    int count = 0;
+    while (s[count] != '\0') {
+        count++;
+    }
+    write(1, s, count);
 }
 
 void hex_format_offset(unsigned offset, char sbuf[]) {
@@ -34,20 +38,33 @@ void hex_format_offset(unsigned offset, char sbuf[]) {
 }
 
 void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]) {
-    unsigned temp_offset = byteval;
-    unsigned temp;
-    sbuf[0] = '0';
-    sbuf[1] = '0';
-    int i = 1;
-    while (temp_offset != 0) {
-        temp = temp_offset % 16;
-        if (temp < 10) {
-            temp += 48;
-        } else {
-            temp += 87;
-        }
-        sbuf[i--] = temp;
-        temp_offset = temp_offset/16;
+    // unsigned temp_offset = byteval;
+    // unsigned temp;
+    // sbuf[0] = '0';
+    // sbuf[1] = '0';
+    // int i = 1;
+    // while (temp_offset != 0) {
+    //     temp = temp_offset % 16;
+    //     if (temp < 10) {
+    //         temp += 48;
+    //     } else {
+    //         temp += 87;
+    //     }
+    //     sbuf[i--] = temp;
+    //     temp_offset = temp_offset/16;
+    // }
+    unsigned temp_byteval = byteval % 16;
+    if (temp_byteval < 10) {
+        sbuf[1] = temp_byteval + 48;
+    } else {
+        sbuf[1] = temp_byteval + 87;
+    }
+    temp_byteval = byteval/16 % 16;
+
+    if (temp_byteval < 10) {
+        sbuf[0] = temp_byteval + 48;
+    } else {
+        sbuf[0] = temp_byteval + 87;
     }
     sbuf[2] = '\0';
 }
