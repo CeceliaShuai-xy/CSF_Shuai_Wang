f717ce1cdbbfbb1134fe0c603dd461844c6d6a7d
Xinyu Shuai <xinyushuaic@gmail.com>
2023-02-17 09:31:30 -0500

    pass the autograder

--- csf_assign02_2_9/c_hexmain.c
+++ csf_assign02_2_9/c_hexmain.c
@@ -5,49 +5,46 @@
 int main(void) {
   // TODO: implement the main function
   unsigned offset = 0;
-  // leave room for null terminator
-  char buff[18]; 
-  buff[17] = '\0';
+  
   // store number of read bytes
   unsigned n = 0; 
+  const char *fmtstr = ":";
+  const char *empty = " ";
+
+
   while (1) {
+    // leave room for null terminator //and newline
+    char buff[18]; 
     // num of bytes read
     n = hex_read(buff);
     if (n == 0) {
       break;
     } 
 
-    char hex_offset[8];
+    char hex_offset[9];
+
     hex_format_offset(offset, hex_offset);
     hex_write_string(hex_offset);
-
-    // string to write
-    // 3 spaces + ':' + 15 inbetween spaces + 16*2 = 51
-    // 51+1 = 52 add one for null terminator
-    char s[52]; 
-    for (int i = 0; i < 52; i++) {
-      s[i] = ' ';
-    }
-    s[0] = ':';
-    s[51] = '\0';
+    hex_write_string(fmtstr);
 
     // string buff
-    char sbuf[2];
-  
-    for (int i = 2; i < n+2; i++) {
-      hex_format_byte_as_hex(buff[i-2],sbuf);
-      for (int j = 0; j < 3; j++) {
-        if (j != 2) {
-          s[3*i+j-4] = hex_to_printable(sbuf[j]);
-        } 
-      }
-    }
-
-    hex_write_string(s);
-
+    char sbuf[3];
     for (int i = 0; i<n; i++) {
+      hex_format_byte_as_hex(buff[i],sbuf);
       buff[i] = hex_to_printable(buff[i]);
+      
+      hex_write_string(empty);
+      hex_write_string(sbuf);
     }
+    // hex_write_string(s);
+    for (int j = n; j < 16; j++) {
+      hex_write_string(empty);
+      hex_write_string(empty);
+      hex_write_string(empty);
+    }
+
+    hex_write_string(empty);
+    hex_write_string(empty);
 
     buff[n] = '\n';
     buff[n+1] = '\0';
