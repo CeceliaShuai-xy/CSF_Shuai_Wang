74aed7668b4b04b34c79b99b4d7e375d8b1b9ec2
ywang580 <ywang580@login01.rockfish.cluster>
2023-03-13 00:41:21 -0400

    add function.cpp

---
+++ csf_assign03/csim_functions.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <exception>
+#include "csim_functions.h"
+#include <sstream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::cerr;
+using std::getline;
+using std::string;
+using std::atoi;
+using std::stoi;
+using std::exception;
+using std::stringstream;
+
+
+
+// check if input_param is valid
+// throw std::invalid_argument() if augments is invalid
+void checkValidAuguments(Input_param input_param);
+
+//Gigi
+Cache initialization_cache(Input_param input_param);
+
+//Cecilla
+Stats initialization_stats();
+
+uint32_t log2(uint32_t number);
+
+//Gigi
+uint32_t findTag(uint32_t address, uint32_t index_pos);
+
+//Cecilla
+uint32_t findIndex(uint32_t address, uint32_t offset_pos);
+
+bool isHit(uint32_t tag, uint32_t index, Cache cache);
+
+void updateLRU(uint32_t tag, uint32_t index, Cache *cache);
+
+// initalize map (tag to slot)
+void putNewSlot(uint32_t tag, uint32_t index, const Input_param input, Cache *cache, Stats stats);
+
