28730c7b6658a324a22d9f99e05921a4318d23d8
ywang580 <ywang580@login03.rockfish.cluster>
2023-03-29 15:33:52 -0400

    perform new experiments

--- csf_assign03/run_exp.py
+++ csf_assign03/run_exp.py
@@ -5,13 +5,13 @@ import itertools
 
 # below are parameters to vary:
 
+sets_size = [1, 1024]
 block_size = [1, 4, 1024]
-sets_size = [256, 1024]
 bytes_size = [16]
 write = ["write-allocate write-back", "no-write-allocate write-through"]
 eviction = ["lru", "fifo"]
 
-list_ = [block_size, sets_size, bytes_size, write, eviction]
+list_ = [sets_size, block_size, bytes_size, write, eviction]
 
 combination = [p for p in itertools.product(*list_)]
 
