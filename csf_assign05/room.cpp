b3cf25a6dd75b9f4c186684f8ca1694579931118
Yiran Wang <you@somewhere.com>
2023-04-29 19:08:34 -0400

    start ms2

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -6,20 +6,25 @@
 
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
+    pthread_mutex_init(&lock, NULL);
   // TODO: initialize the mutex
-  
 }
 
 Room::~Room() {
+  pthread_mutex_destroy(&lock);
   // TODO: destroy the mutex
 }
 
 void Room::add_member(User *user) {
   // TODO: add User to the room
+  Guard g(lock);
+  members.insert(user);
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
+  Guard g(lock);
+  members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -13,6 +13,8 @@
 #include "guard.h"
 #include "server.h"
 
+#include "csapp.h"
+
 ////////////////////////////////////////////////////////////////////////
 // Server implementation data types
 ////////////////////////////////////////////////////////////////////////
@@ -23,6 +25,20 @@
 ////////////////////////////////////////////////////////////////////////
 // Client thread functions
 ////////////////////////////////////////////////////////////////////////
+
+struct ConnInfo{
+  Connection* connection;
+  Server *server;
+
+  ConnInfo(Connection* connection, Server* server) {
+    this->connection = connection;
+    this->server = server;
+  }
+
+  ~ConnInfo() {
+    delete connection;
+  }
+};
 
 namespace {
 
@@ -43,6 +59,20 @@ void *worker(void *arg) {
 
   return nullptr;
 }
+}
+
+void Server::chat_with_sender(Connection* connection, Server* server, std::string username) {
+  Message msg;
+  Room* room = nullptr;
+  while (1) {
+    if(!connection->receive(msg)){
+      connnection->send(Message(TAG_ERR,"invalid message"));
+    }
+  }
+
+}
+
+void Server::chat_with_receiver(Connection* connection, Server* server, std::string username) {
 
 }
 
@@ -54,23 +84,52 @@ Server::Server(int port)
   : m_port(port)
   , m_ssock(-1) {
   // TODO: initialize mutex
+  pthread_mutex_init(&m_lock, NULL);
 }
 
 Server::~Server() {
   // TODO: destroy mutex
+  pthread_mutex_destroy(&m_lock);
 }
 
 bool Server::listen() {
   // TODO: use open_listenfd to create the server socket, return true
   //       if successful, false if not
+  int fd = open_listenfd(std::to_string(m_port).c_str());
+
+  if (fd < 0) {
+    return false;
+  }
+  m_ssock = fd;
+  return true;
 }
 
 void Server::handle_client_requests() {
   // TODO: infinite loop calling accept or Accept, starting a new
   //       pthread for each connected client
+  while (1) {
+    int clientfd = Accept(m_ssock, NULL, NULL);
+    if (clientfd < 0) {
+      fprintf(stderr, "%s\n","Connot accept client connection");
+      exit(1);
+    }
+
+    Connection* connection = new Connection(clientfd);
+    ConnInfo* info = new ConnInfo(connection, this);
+    pthread_t thr_id;
+    if (pthread_create(&thr_id, NULL, worker, info) != 0) {
+      fprintf(stderr, "%s\n","Connot create thread");
+      exit(1);
+    }
+  }
 }
 
 Room *Server::find_or_create_room(const std::string &room_name) {
   // TODO: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
+  Guard g(m_lock);
+  if (m_rooms.find(room_name) == m_rooms.end()) {
+    m_rooms[room_name] = new Room(room_name);
+  }
+  return m_rooms.at(room_name);
 }
